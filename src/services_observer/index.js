aca9ab97cf7ab3114aac6777eb50696a77890b35
Omar <roma4053@gmail.com>
2021-11-14 17:21:12 +0300

    web socket

--- src/services_observer/index.js
+++ src/services_observer/index.js
@@ -1,17 +1,35 @@
-var request = require('request');
 var config = require('./config.json');
 var Observer = require('./Observer');
 
+const WebSocket = require('ws');
 
 
-async function main(){
+const wsServer = new WebSocket.Server({ port: 3000 });
 
-    let observer = new Observer(config)
-    for (const obj of await observer.checkServices()) {
-        for (const page of obj.pages) {
-            console.log(page)
-        }
+let observer = new Observer(config)
+var clients = []
+wsServer.on('connection', function(ws) {
+
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
+  
+    ws.on('message', function(message) {});
+
+    ws.on("error", (err) => {
+        console.log(err.stack);
+      });
+  
+    ws.on('close', function() {
+      console.log('соединение закрыто ' + id);
+      delete clients[id];
+    });
+  
+  });
+setInterval(async() => {
+    for (const wsClient of wsServer.clients) {
+        wsClient.send(JSON.stringify(await observer.checkServices()))
     }
-    
-}
-setInterval(main, 10000)
+}, 10000);
+
+
