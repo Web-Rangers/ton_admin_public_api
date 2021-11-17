9dabfe2a6b71426048a7cc823fee218f61403b0d
Ramzan <47396837+SharkiKong@users.noreply.github.com>
2021-11-17 18:20:37 +0300

    Merge branch 'main' of https://github.com/Web-Rangers/ton_admin_server

--- src/request/validator/elections.js
+++ src/request/validator/elections.js
@@ -2,7 +2,6 @@ let jrpc = require('../../data/json_rpc_status')
 
 
 function get_elections_data() {
-    console.log(jrpc.status);
     return {'id': jrpc.electionId,'start':jrpc.startElection,
             'end': jrpc.endElection, 'next': jrpc.startNextElection}
 }
