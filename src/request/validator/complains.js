cc3b31351337eb2e7f810f7acfd1692823f9c47e
Ramzan <47396837+SharkiKong@users.noreply.github.com>
2021-11-17 22:52:05 +0300

    Update complains.js

--- src/request/validator/complains.js
+++ src/request/validator/complains.js
@@ -2,7 +2,7 @@
 let jrpc = require('../../data/json_rpc_status')
 
 async function get_complaints() {
-    return {'validators': jrpc.status.complains}
+    return {'complains': jrpc.status.complains}
 }
     
 module.exports = {get_complaints}
