d7b895b65a597bc8dc631ba9f876e8a1c7c26feb
Ramzan <47396837+SharkiKong@users.noreply.github.com>
2021-11-23 18:30:40 +0300

    User model delete.

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -2,6 +2,5 @@ const {Block, BlockTransaction} = require('./block')
 const {Bridge, TonBridgeTransaction, Web3BridgeTransaction} = require('./bridge')
 const {Server} = require('./server')
 const {Service, Page} = require('./service')
-const {User} = require('./user')
 
-module.exports = {Block, BlockTransaction, Bridge, TonBridgeTransaction, Web3BridgeTransaction, Server, Service, Page, User}
+module.exports = {Block, BlockTransaction, Bridge, TonBridgeTransaction, Web3BridgeTransaction, Server, Service, Page}
--- src/db/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = mongoose.Schema({
-    ip: { type: String, required: true }
-})
-
-const User = mongoose.model('User', userSchema)
-module.exports = { User }
