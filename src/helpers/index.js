914e37e4405c9879a586ccd86b7c98edf5e097ad
Danila <42590150+danpinkB@users.noreply.github.com>
2021-11-23 16:09:46 +0300

    broke

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,6 +3,5 @@ const {status_requester} = require('./json_rpc/status_requester')
 const {block_monitor} = require('./blocks')
 const {servers_monitor} = require('./servers/server_monitor')
 const {service_monitor} = require('./services/service_monitor')
-const {auth_service} = require('./auth/auth_service')
 
-module.exports = {bridges_monitor,status_requester,block_monitor,servers_monitor,service_monitor,auth_service}
+module.exports = {bridges_monitor,status_requester,block_monitor,servers_monitor,service_monitor}
