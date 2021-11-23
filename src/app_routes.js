e72baf4289457203efd0e1e2866f93678d827f41
Danila <42590150+danpinkB@users.noreply.github.com>
2021-11-23 15:47:02 +0300

    broke

--- src/app_routes.js
+++ src/app_routes.js
@@ -1,10 +1,9 @@
-const {bridge_router,metric_router,service_router,admin_router} = require('./routes')
+const {bridge_router,metric_router,service_router} = require('./routes')
 
 function build_routes(app) {
     app.use('/api/bridge',bridge_router)
     app.use('/api/metric',metric_router)
     app.use('/api/service',service_router)
-    app.use('/admin',admin_router)
 }
 
 module.exports = {build_routes}
--- src/controller/ton_admin/index.js
+++
@@ -1,3 +0,0 @@
-const {build_admin_controller} = require('./ton_admin_controller')
-
-module.exports = {build_admin_controller}
