e799b3ba3be5b3362ffb5807663907cbe7ac102b
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-01-19 13:39:36 +0300

    change_complains

--- monitoring/src/db/operations/service/update_service_page.js
+++ monitoring/src/db/operations/service/update_service_page.js
@@ -2,8 +2,10 @@ import db_connection from '../../dbaccess/db_connection'
 
 async function update_service(name, page){
     let timestamp = Math.round(new Date().getTime()/1000)
-    db_connection.connection.execute(`INSERT INTO status_services (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status}) ON DUPLICATE KEY UPDATE time=${timestamp},latency=${page.response_time},status_code=${page.response_status}`)  
-    db_connection.connection.execute(`INSERT INTO service_ping (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status});`)  
-}
+    if (page.response_time&&page.response_status){
+        db_connection.connection.execute(`INSERT INTO status_services (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status}) ON DUPLICATE KEY UPDATE time=${timestamp},latency=${page.response_time},status_code=${page.response_status}`)  
+        db_connection.connection.execute(`INSERT INTO service_ping (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status});`) 
+    }
+   }
 
 export {update_service}
