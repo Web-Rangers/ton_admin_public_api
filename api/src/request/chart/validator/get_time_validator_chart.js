e52b3bd1977f4da4a8e80118449dceb45aea94c4
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-02-02 15:45:28 +0300

    req_fix

--- api/src/request/chart/validator/get_time_validator_chart.js
+++ api/src/request/chart/validator/get_time_validator_chart.js
@@ -3,7 +3,7 @@ import get_time from '../../../helpers/time_wizard'
 
 async function get_validator_period_chart(wallet,time_period,time_value) {
     let time = get_time(time_period,1)
-    let now = Math.round(new Date().getTime()/1000)-time[0]*time_value
+    let now = Math.round(new Date().getTime()/1000)-time[0]*time[1]*time_value
     let [rows] = await db_connection.connection.query(`SELECT date, increase as value FROM ton_status.validators_history where walletAddr='${wallet}' and date >=${now} order by date;`)
     return rows
 }
