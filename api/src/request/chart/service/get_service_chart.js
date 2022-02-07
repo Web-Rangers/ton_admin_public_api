15294994470b2f46912210b9ba055241888b675b
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-02-07 15:15:15 +0300

    space_fix

--- api/src/request/chart/service/get_service_chart.js
+++ api/src/request/chart/service/get_service_chart.js
@@ -4,7 +4,8 @@ import get_time from '../../../helpers/time_wizard'
 async function get_service_chart(service_name,time_period,time_value) {
     let time = get_time(time_period,time_value)
     let now = Math.round(new Date().getTime()/1000)-time[0]*time[1]
-    let [rows] = await db_connection.connection.query(`SELECT ROUND((UNIX_TIMESTAMP() - time)/(${time[0]})) as date,avg(latency)as value from service_ping WHERE service_name='${service_name}' and time >= ${now}and status_code=200 group by date;`)
+    let [rows] = await db_connection.connection.query(`SELECT ROUND((UNIX_TIMESTAMP() - time)/(${time[0]})) as date,avg(latency)as value from service_ping WHERE service_name='${service_name}' and time >= ${now} and status_code=200 group by date;`)
+    
     return rows
 }
 
