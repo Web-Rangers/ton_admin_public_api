60f21244b337082d576b31f7cd10a19bb2de992e
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-02-08 18:04:55 +0300

    backworkers,timeout

--- admin/src/request/admin/proxy/proxy_request.js
+++ admin/src/request/admin/proxy/proxy_request.js
@@ -3,14 +3,15 @@ import axios from 'axios'
 async function tonadmin_proxy(request_params){
     let headers = {
         'Content-Type': 'application/json'
-    }
+    };
+    
     let {token,url,jsonrpc,id,method,params} = {...request_params.body}
     if (token){
         headers['Authorization'] = "token " + token
     }
     return axios.post(
         url+'/',
-        JSON.stringify({jsonrpc: jsonrpc, id: id, method: method, params: params}),{headers:headers}
+        JSON.stringify({jsonrpc: jsonrpc, id: id, method: method, params: params}),{headers:headers,timeout:10000}
     )
 }
 
