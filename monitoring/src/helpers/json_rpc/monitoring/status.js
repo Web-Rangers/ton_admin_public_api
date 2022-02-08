597f4eaa019f8f43e3c5743540a71225c10dca2c
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-02-08 18:46:25 +0300

    callbacksh

--- monitoring/src/helpers/json_rpc/monitoring/status.js
+++ monitoring/src/helpers/json_rpc/monitoring/status.js
@@ -9,7 +9,8 @@ async function get_status() {
         let res = result.data.result
         let start_end = db_connection.connection.execute('SELECT startValidation,endValidation from status',(err,result)=>{
             if (result.length>0&&res.endValidation>result[0].endValidation){
-                exec('sh ~/analyze.sh',(err,s,se)=>{
+                
+                exec('sh ./analyze.sh',(err,s,se)=>{
                     console.log(err,s,se);
                 })
                 db_connection.connection.execute(`INSERT IGNORE INTO validators_cycle_history (date_start,date_end) VALUES(${result[0].startValidation},${result[0].endValidation})`)
