0f3ac4d22f53cfa4bad8170eeee6ca57026a89bc
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-02-22 18:04:52 +0300

    change validator analyze

--- monitoring/src/fetch/json_rpc/monitoring/analyze_validators.js
+++ monitoring/src/fetch/json_rpc/monitoring/analyze_validators.js
@@ -12,6 +12,7 @@ const db_connection = mysql.createConnection({
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const elector_contract = "Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF"
+let last_in = undefined
 const analyze_validator = async () => {
     let txs = [];
     axios.default.defaults.headers['Accept']='*/*'
@@ -21,80 +22,71 @@ const analyze_validator = async () => {
 
     db_connection.execute('SELECT * FROM status_validators',async(err,res)=>{
         for (let validator of res) {
-            await delay(2000)
+            await delay(1000)
             try {
-                validator.walletAddr='Ef-4wGDdtaSFk7gWezRrHRTbBGW6X5FLVDBkHkzxZjZLXUtQ'
-                let transactions = await axios.get(`https://api.ton.cat/v2/explorer/getTransactions?address=${validator.walletAddr}&limit=130&archival=false`,{timeout: 3000})
+                let transactions = await axios.get(`https://api.ton.cat/v2/explorer/getTransactions?address=${validator.walletAddr}&limit=200&archival=false`,{timeout: 3000})
                 txs = transactions.data.result
+                txs = txs.filter(tx=>(tx.out_msgs && tx.out_msgs[0] && tx.out_msgs[0].destination == elector_contract && tx.out_msgs[0].value/10**9 > 1000) || (tx && tx.in_msg.source == elector_contract && tx.in_msg.value/10**9 > 1000))
                 let in_ = []
+                let elector_pre_stake = []
                 let last_action = undefined
+                last_in = undefined
                 if(txs.length > 0){
-                
-                    let in_transactions= txs.filter(tx=>(tx && tx.in_msg.source == elector_contract && tx.in_msg.value/10**9 > 15))
-                    let out_transactions= txs.filter(tx=>(tx.out_msgs && tx.out_msgs[0] && tx.out_msgs[0].destination == elector_contract && tx.out_msgs[0].value/10**9 > 15))
                     console.log(validator.walletAddr);
                     
-                    for (const tx of in_transactions) {
-                        tx.in_msg.value =  tx.in_msg.value/10**9
-                        let txs_out = out_transactions.filter(t=>(Math.abs(tx.utime-t.utime)/(60*60)>35)&&(Math.abs(tx.utime-t.utime)/(60*60)<37)).map(x=>x={'utime':Math.abs(tx.utime-x.utime)/(60*60),'value':x.out_msgs[0].value/10**9})
-                        console.log(tx.utime,tx.in_msg.value);
-                        
-                        console.log(txs_out);
-                        for (let i = txs_out.length-1; i >= 0; i--) {
-                            let txo = txs_out[i];
-                            if (tx.in_msg.value>0){
-                                tx.in_msg.value-=txo.value
+                    for (let i = 0; i < txs.length; i++) {
+                        let tx = txs[i];
+                        if (tx.in_msg.source == elector_contract){
+                            console.log(tx.in_msg.value/(10**9), tx.utime);
+                            if (last_action != 'IN'){
+                                last_action='IN'
+                                insert(in_,validator)
+                                in_.push({date:tx.utime,val:tx.in_msg.value/(10**9)})
+                            }
+                            else{
+                                if((in_[in_.length-1].val/(tx.in_msg.value/(10**9))>9)){
+                                    elector_pre_stake.push(tx.in_msg.value/(10**9))
+                                }
+                                else{
+                                    in_.push({date:tx.utime,val:tx.in_msg.value/(10**9)})  
+                                }
                             }
                         }
-                        console.log(tx.in_msg.value);
-                        
-                    }
-                    // for (let i = 0; i < txs.length; i++) {
-                    //     let tx = txs[i];
-                    //     if (tx.in_msg.source == elector_contract){
-                    //         console.log(tx.in_msg.value/(10**9), tx.utime);
-                    //         if (last_action != 'IN'){
-                    //             last_action='IN'
-                    //             if ((in_.length>1)){
-                    //                 let dv = in_.shift() 
-                                     
-                    //                 try {
-                    //                     db_connection.execute(`INSERT INTO validators_history (adnlAddr,walletAddr,date,increase) VALUES('${validator.adnlAddr}','${validator.walletAddr}',${dv.date},${dv.val}) ON DUPLICATE KEY UPDATE increase=${dv.val}`)      
-                    //                 } catch (error) {
-                    //                     console.log('continueeeeeeeee');
-                    //                     break
-                    //                 }  
-                    //             }
-                    //             in_.push({date:tx.utime,val:tx.in_msg.value/(10**9)})
-                    //         }
-                    //         else{
-                    //             in_[in_.length-1].val+= tx.in_msg.value/(10**9)
-                    //             console.log('aaaaa',in_[in_.length-1]);
-                    //         }
-                    //     }
-                    //     else if (tx.out_msgs&&tx.out_msgs[0]&&tx.out_msgs[0].destination == elector_contract){
-                    //         last_action='OUT'
-                    //         console.log(tx.out_msgs[0].value/(10**9));
-                    //         if (in_[0]) {
-                    //             console.log((in_[0].date-tx.utime)/(60*60));
-                    //             if (((in_[0].date-tx.utime)/(60*60)>=35)&&((in_[0].date-tx.utime)/(60*60)<42)){ 
-                    //                 in_[0].val-= tx.out_msgs[0].value/(10**9)
-                    //             } 
-                    //             else{
-                    //                 let dv = in_.shift() 
-                                    
-                    //                 if (dv.val<10000){ 
-                    //                     try {
-                    //                         db_connection.execute(`INSERT INTO validators_history (adnlAddr,walletAddr,date,increase) VALUES('${validator.adnlAddr}','${validator.walletAddr}',${dv.date},${dv.val}) ON DUPLICATE KEY UPDATE increase=${dv.val}`)      
-                    //                     } catch (error) {
-                    //                         console.log('continueeeeeeeee');
-                    //                         break
-                    //                     }
-                    //                 }
-                    //             }
-                    //         }                
-                    //     }
-                    // }      
+                        else if (tx.out_msgs&&tx.out_msgs[0]&&tx.out_msgs[0].destination == elector_contract){
+                            last_action='OUT'
+                            console.log(tx.out_msgs[0].value/(10**9));
+                            if (in_[1]) { 
+                                if (in_[0].val-tx.out_msgs[0].value/(10**9)<-1000){
+                                    if(elector_pre_stake.length>0){
+                                        if((in_[0].val+elector_pre_stake[0]-tx.out_msgs[0].value/(10**9))>-1000){
+                                            in_[0].val-= tx.out_msgs[0].value/(10**9)
+                                            in_[0].val+=elector_pre_stake.pop()
+                                            insert(in_,validator)
+                                        }
+                                        else{
+                                            in_[1].val-= tx.out_msgs[0].value/(10**9)
+                                            in_[1].val+=elector_pre_stake.pop()
+                                            insert(in_,validator) 
+                                        } 
+                                        continue
+                                    }
+                                    in_[1].val-= tx.out_msgs[0].value/(10**9)
+                                    insert(in_,validator)     
+                                    continue
+                                }
+                                in_[0].val-= tx.out_msgs[0].value/(10**9)
+                            }
+                            else if(in_[0]){
+                                console.log((in_[0].date-tx.utime)/(60*60));
+                                if(((in_[0].date-tx.utime)/(60*60))>=36){
+                                    in_[0].val-= tx.out_msgs[0].value/(10**9)
+                                    if ((in_[0].val<0)&&(elector_pre_stake.length>0)){
+                                        in_[0].val+=elector_pre_stake.pop()
+                                    }
+                                }
+                            }               
+                        }
+                    }      
                 } 
             } catch (error) {
                 console.log(error);
@@ -102,5 +94,18 @@ const analyze_validator = async () => {
             }
         }
     })
+}
+
+function insert(in_,validator){
+    if ((in_.length>1)){
+        let dv = in_.shift() 
+        if (last_in&&(Math.abs(dv.val)/Math.abs(last_in_))>9)return
+        if (!last_in){last_in=Math.abs(dv.val)}
+        try {
+            db_connection.execute(`INSERT INTO validators_history (adnlAddr,walletAddr,date,increase) VALUES('${validator.adnlAddr}','${validator.walletAddr}',${dv.date},${dv.val}) ON DUPLICATE KEY UPDATE increase=${dv.val}`)      
+        } catch (error) {
+            console.log('continueeeeeeeee');
+        }  
+    }
 }
 analyze_validator()
