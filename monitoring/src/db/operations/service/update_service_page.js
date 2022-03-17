d09096b12fbf304c426daad915035194e27edccf
Belaz <42590150+danpinkB@users.noreply.github.com>
2022-03-17 14:17:36 +0400

    pool

--- monitoring/src/db/operations/service/update_service_page.js
+++ monitoring/src/db/operations/service/update_service_page.js
@@ -2,7 +2,7 @@ import db_connection from '../../dbaccess/async_connection'
 
 async function update_service(name, page){
     let timestamp = Math.round(new Date().getTime()/1000)
-    if (page.response_time&&page.response_status){
+    if (page.response_time&&page.response_status&&page.response_time<5000){
         await db_connection.connection.execute(`INSERT INTO status_services (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status}) ON DUPLICATE KEY UPDATE time=${timestamp},latency=${page.response_time},status_code=${page.response_status}`).catch(err=>{console.log(err);});  
         await db_connection.connection.execute(`INSERT INTO service_ping (service_name,page_name,time,latency,status_code) values ('${name}','${page.name}',${timestamp},${page.response_time},${page.response_status});`).catch(err=>{console.log(err);}); 
     }
